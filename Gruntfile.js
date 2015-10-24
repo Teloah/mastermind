7e37334f5a87146245c0109b790a68687a7b6dcc
Andris Klaipiņš <teloah@gmail.com>
2015-10-24 23:53:26 +0300

    Added spritesmith grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,15 @@
 module.exports = function(grunt) {
   grunt.initConfig({
-    jshint: ['Gruntfile.js']
+    jshint: ['Gruntfile.js', 'mastermind.js'],
+    sprite: {
+      all: {
+        src: 'images/*.png',
+        dest: 'build/images/icons.png',
+        destCss: 'build/css/icons.css'
+      }
+    }
   });
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-spritesmith');
   grunt.registerTask('default', ['jshint']);
 };
